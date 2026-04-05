453f8cb363df9e745039040c2fcd8240ebb9df2e
james-e-crandall <james.e.crandall@gmail.com>
2026-04-05 09:58:03 -0500

    add home page, add base images

--- coffee-website/src/app/app.routes.ts
+++ coffee-website/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home-page/home-page';
 
-export const routes: Routes = [];
+const HomePageRoute = { path: '', component: HomePage, title: 'Home Page' };
+const NotFoundRoute = { path: '**', component: HomePage, title: 'Home Page' };
+
+export const routes: Routes = [
+  HomePageRoute,
+  NotFoundRoute
+];
