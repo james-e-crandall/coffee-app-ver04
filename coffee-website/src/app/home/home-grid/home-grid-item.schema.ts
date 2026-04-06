8f22b03dbcca3064628a5cc5a27f65ef817e95da
james-e-crandall <james.e.crandall@gmail.com>
2026-04-06 15:11:49 -0500

    home grid

--- coffee-website/src/app/home/home-grid/home-grid-item.schema.ts
+++ coffee-website/src/app/home/home-grid/home-grid-item.schema.ts
@@ -1,8 +1,13 @@
+import { SafeValue } from "@angular/platform-browser";
 
+export interface HomeGridItemImage {
+  imageSrc: string | SafeValue;
+}
 export interface HomeGridItem {
   type: 'image' | 'button' | 'text';
   title: string;
   description: string;
   colorClass:string;
   columnClass:string;
+  image: string
 }
--- coffee-website/src/app/home/home-grid/home-grid-service.ts
+++ coffee-website/src/app/home/home-grid/home-grid-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HomeGridItem } from './home-grid-item.schema';
 
-
 @Injectable({
   providedIn: 'root',
 })
@@ -12,28 +11,32 @@ export class HomeGridService {
       title: 'Acme Fresh Start Housing',
       description: 'Chicago',
       colorClass:'green-theme',
-      columnClass:'two-column'
+      columnClass:'two-column',
+      image:''
     },
     {
       type: 'button',
       title: 'Acme Fresh Start Housing',
       description: 'Chicago',
       colorClass:'green-theme',
-      columnClass:'two-column'
+      columnClass:'two-column',
+      image:''
     },
     {
       type: 'image',
       title: 'Acme Fresh Start Housing',
       description: 'Chicago',
       colorClass:'green-theme',
-      columnClass:'two-column'
+      columnClass:'two-column',
+      image:'/images/sunset03.png'
     },
     {
       type: 'text',
       title: 'Acme Fresh Start Housing',
       description: 'Chicago',
       colorClass:'orange-theme',
-      columnClass:'one-column'
+      columnClass:'one-column',
+      image:''
     },
   ];
 
